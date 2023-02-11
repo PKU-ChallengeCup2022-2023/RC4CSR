3849bcb8d776e1b9f94dcf085252f53b4adaaa89
MOLYHECI <molyheci@outlook.com>
2023-02-11 17:53:37 +0800

    Add editinfo page;

--- System/Account/urls.py
+++ System/Account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", views.UserLogin, name="login"),
     path("changepw/", views.Change_Password, name="chagnepw"),
     path("<slug:username>/", views.UserPage),
+    path("<slug:username>/editinfo/", views.Edit_Info, name="editinfo"),
 ]
